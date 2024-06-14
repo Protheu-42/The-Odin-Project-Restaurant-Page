fc2385c9ea56edcd6524ca6c3376e34755b23d90
Protheu <everaldo.sobrinho42@gmail.com>
2024-06-14 13:06:12 -0300

    Basic config and the Layout in HTML, style create

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # The-Odin-Project-Restaurant-Page
+
+<a href="https://www.freepik.com/free-vector/hand-drawn-brewery-logo-design_37442705.htm#fromView=search&page=1&position=1&uuid=b627ef69-303f-4360-a0cf-70b67998cf65">Image by freepik</a>
---
+++ src/homepageLoader.js
@@ -0,0 +1,27 @@
+export default function () {
+    const contentDiv = document.querySelector('#content')
+
+    const firstDiv = document.createElement('div');
+
+    // H1's
+    const firstH1 = document.createElement('h1');
+    firstH1.textContent = 'Craft';
+    const secondH1 = document.createElement('h1');
+    secondH1.textContent = 'Brewery';
+
+    const companyIntro = document.createElement('p');
+    companyIntro.textContent = 'Welcome to Hopster Brews! Crafting unique flavors, pouring passion into every pint. Explore our artisanal ales, savor the taste of handcrafted perfection. Cheers to unforgettable moments and exceptional brews!';
+
+    const secondDiv = document.createElement('div');
+    logo.id = 'logo';
+
+    //Appending Child Section
+    contentDiv.appendChild(firstDiv);
+    
+    //firtDiv Childs
+    firstDiv.appendChild(firstH1);
+    firstDiv.appendChild(secondH1);
+    firstDiv.appendChild(companyIntro);
+
+    contentDiv.appendChild(secondDiv);
+}
