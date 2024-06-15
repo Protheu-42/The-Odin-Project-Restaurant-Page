c2b78e9ef71b3bff516990cbf61d261eeac21e5f
Protheu <everaldo.sobrinho42@gmail.com>
2024-06-15 10:48:36 -0300

    Home contact tab navigation create, style fix index.js tabNav logic create, minnor alignment in style fix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ import './style.css';
 import homepageLoader from './homepageLoader.js';
 import contactpageLoader from './contactpageLoader.js';
 
-// homepageLoader()
-// contactpageLoader();
+const NavBtnListeners = (function() {
+    const contentDiv = document.querySelector('#content')
+    const homeBtn = document.querySelector('#navHome');
+    const menuBtn = document.querySelector('#navMenu');
+    const contactBtn = document.querySelector('#navContact');
+
+    homeBtn.addEventListener('click', () =>{
+        cleanAndLoad();
+        homepageLoader();
+    });
+    contactBtn.addEventListener('click', () =>{
+        cleanAndLoad();
+        contactpageLoader();
+    });
+
+    function cleanAndLoad() {
+        while (contentDiv.firstChild) {
+            contentDiv.removeChild(contentDiv.firstChild);
+        };
+    }
+})();
+
+homepageLoader()
